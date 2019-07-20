6aa0e8c2654b8c9bdcf4e50d08c1b6f400e1a034
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2019-07-20 11:54:58 +0700

    ok

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+include "database.php";
+session_start();
+
+$action = null;
+
+if (!isset($_REQUEST['act'])){
+   $action = null; 
+}else{
+   $action = $_REQUEST['act'];  
+}
+
+$d = new Database();
+$d->open();
+
+$resArr = array();
+
+if ($action == null){
+	$resArr[] = array("result" => "0");
+	print json_encode($resArr);
+}elseif($action == "1"){
+    $sql = "select * from pengguna where username = '". $_REQUEST['username'] ."' and "
+            ."password = '". md5($_REQUEST['password'])."'";
+			
+	$res = $d->get($sql);
+	
+	//apakah ada data?
+	if ($res[0]['username'] != "" && $res[0]['nama'] != ""){
+		//daftarkan session pada sisi server (PHP)
+		$_SESSION["usr"] = $res[0]['username'];
+		$_SESSION["nm"] = $res[0]['nama'];	
+	}
+	
+	$resArr[] = array("result" => $res);
+	print json_encode($resArr);
+}
+?>
---
+++ pengguna.php
@@ -0,0 +1,61 @@
+<?php
+include "database.php";
+
+$action = null;
+
+if (!isset($_REQUEST['act'])){
+   $action = null; 
+}else{
+   $action = $_REQUEST['act'];  
+}
+
+$d = new Database();
+$d->open();
+
+$resArr = array();
+
+if ($action == null){	
+	$resArr[] = array("result" => "0");
+	print json_encode($resArr);
+}elseif($action == "1"){
+    $sql = "insert into pengguna (nama, username, password) "
+			. "values ('". $_REQUEST['nama']."', '". $_REQUEST['username']."', "
+			. "'". md5($_REQUEST['password'])."')";
+
+    $d->execute($sql);
+    
+	$resArr[] = array("result" => "data inserted");
+	print json_encode($resArr);
+}elseif($action == "2"){
+    if ($_REQUEST['password'] == ""){
+        $sql = "update pengguna set nama = '". $_REQUEST['nama']."' "
+				. "where username = '". $_REQUEST['username']."'";
+    }else{
+        $sql = "update pengguna set password = '". md5($_REQUEST['password'])."', "
+				. "nama = '". $_REQUEST['nama']."' "
+				. "where username = '". $_REQUEST['username']."'";
+    }
+	
+	$d->execute($sql);
+
+    
+    $resArr[] = array("result" => "data updated");
+	print json_encode($resArr);
+}elseif($action == "3"){
+    $sql = "delete from pengguna where username ='". $_REQUEST['username'] ."'";
+    $d->execute($sql);
+	
+    $resArr[] = array("result" => "data deleted");
+	print json_encode($resArr);
+}elseif($action == "4"){
+    $sql = "select username, nama from pengguna";
+    
+	$resArr[] = array("result" => $d->get($sql));
+	print json_encode($resArr);
+}elseif($action == "5"){
+    $sql = "select username, nama from pengguna where username like '%". $_REQUEST['find'] ."%'";
+    
+	$resArr[] = array("result" => $d->get($sql));
+	print json_encode($resArr);
+}
+?>
